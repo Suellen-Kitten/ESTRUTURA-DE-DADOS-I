ce5a4c37875174cad69f48447520981b726a514b
Suellen Miranda Amor <Suellen Miranda Amor@DESKTOP-ETFVC25>
2021-08-23 16:09:04 -0300

    Adicionar arquivos de projeto.

---
+++ CC4M/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CC4M_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int x = Int32.Parse(input);
+            Console.WriteLine($"O valor digitado foi {x}. Blz? Alguma dúvida?");
+        }
+    }
+}
